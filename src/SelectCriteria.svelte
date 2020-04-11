b5829adc59abc3d7fbdfb5b028184c51cc546bd4
pntbr <pntbr@pm.me>
2020-04-12 00:31:09 +0200

    enlève l'intitulé thé et ne propose rien par défaut

--- src/SelectCriteria.svelte
+++ src/SelectCriteria.svelte
@@ -5,10 +5,11 @@
     const teas = brewList.filter(tea => tea.type === $type)
 
     const slugifyTranslation = expression =>
-        `${expression.cn} - ${expression.pinyin} - (thé ${expression.fr})`
+        `${expression.cn} - ${expression.pinyin} - (${expression.fr})`
 </script>
 
 <select name="criteria-list">
+    <option disabled selected>-- sélectionner un critère --</option>
     {#each teas as tea, index}
         <option value="{index}">{slugifyTranslation(tea.criteria[0])}</option>
     {/each}
