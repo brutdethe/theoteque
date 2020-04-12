13bc1b77d674c185356eed9d80c81a1b4cf1176c
pntbr <pntbr@pm.me>
2020-04-12 22:38:58 +0200

    reprise de la structure des données

--- src/teaData.js
+++ src/teaData.js
@@ -1,21 +1,36 @@
 const typeList = {
     green: { cn: 'lǜ chá', pinyin: '綠茶', fr: 'vert' },
-    yellow: { cn: 'huáng chá', pinyin: '黃茶', fr: 'jaune' },
     white: { cn: 'bái chá', pinyin: '白茶', fr: 'blanc' },
+    yellow: { cn: 'huáng chá', pinyin: '黃茶', fr: 'jaune' },
     blue: { cn: 'qīng chá', pinyin: '青茶', fr: 'bleu' },
     red: { cn: 'hóng chá', pinyin: '紅茶', fr: 'rouge' },
-    black: { cn: 'hēi chá', pinyin: '紅茶', fr: 'noir' }
+    black: { cn: 'hēi chá', pinyin: '黑茶', fr: 'noir' }
+}
+
+const brewMethodList = {
+    gaiwan: { cn: 'pǐn chá', pinyin: '品茶', fr: 'gaiwan' },
+    zhong: { cn: 'zhōng', pinyin: '盅', fr: 'zhong' },
+    teapot: { cn: 'gōng fū chá', pinyin: '功夫茶', fr: 'gong fu cha' },
+    granpa: { cn: 'bei pao', pinyin: '', fr: 'classique' },
+    kettle: { cn: '', pinyin: '', fr: 'bouilloire' }
 }
 
 const brewList = [
     {
         type: 'green',
         criteria: [{ cn: 'dān yá', pinyin: '單芽', fr: '1 bourgeon' }],
-        temperature: [75, 85],
-        quantity: '1:50',
-        duration: [30, 40],
-        times: '2 fois ou plus',
-        method: 'gaiwan'
+        brewing: {
+            gaiwan: {
+                temperature: [75, 80],
+                quantity: '1:30',
+                duration: [25, 30, 45],
+                times: '2 fois ou plus'
+            },
+            granpa: {
+                temperature: [75, 80],
+                quantity: '1:50'
+            }
+        }
     },
     {
         type: 'green',
@@ -26,11 +41,18 @@ const brewList = [
                 fr: '1 bourgeon et 1, 2, ou 3 feuilles'
             }
         ],
-        temperature: [75, 85],
-        quantity: '1:50',
-        duration: [30, 40],
-        times: '2 fois ou plus',
-        method: 'gaiwan'
+        brewing: {
+            gaiwan: {
+                temperature: [80, 85],
+                quantity: '1:30',
+                duration: [20, 30, 40, 60],
+                times: '2 fois ou plus'
+            },
+            'bei pao': {
+                temperature: [80, 85],
+                quantity: '1:50'
+            }
+        }
     },
     {
         type: 'white',
@@ -41,11 +63,14 @@ const brewList = [
                 fr: '1 bourgeon et 1 feuille ou bourgeon seul'
             }
         ],
-        temperature: [90, 100],
-        quantity: '1:20',
-        duration: [30, 60, 120],
-        times: '5 fois ou plus',
-        method: 'gaiwan'
+        brewing: {
+            gaiwan: {
+                temperature: [90],
+                quantity: '1:20',
+                duration: [30, 40, 50, 60, 70, 90],
+                times: '5 fois ou plus'
+            }
+        }
     },
     {
         type: 'white',
@@ -56,11 +81,14 @@ const brewList = [
                 fr: '1 bourgeon et 1 ou 2 feuilles'
             }
         ],
-        temperature: [80, 85],
-        quantity: '1:50',
-        duration: [120, 180, 240],
-        times: '3 fois ou plus',
-        method: 'theiere'
+        brewing: {
+            gaiwan: {
+                temperature: [95],
+                quantity: '1:30',
+                duration: [30, 40, 50, 60],
+                times: '4 fois ou plus'
+            }
+        }
     },
     {
         type: 'white',
@@ -71,11 +99,14 @@ const brewList = [
                 fr: '1 bourgeon et 3 feuilles'
             }
         ],
-        temperature: [80, 85],
-        quantity: '1:50',
-        duration: [120, 180, 240],
-        times: '3 fois ou plus',
-        method: 'theiere'
+        brewing: {
+            gaiwan: {
+                temperature: [100],
+                quantity: '1:30',
+                duration: [50, 60, 80, 100],
+                times: '4 fois ou plus'
+            }
+        }
     },
     {
         type: 'yellow',
@@ -86,72 +117,156 @@ const brewList = [
                 fr: '1 bourgeon et 1, 2 ou 3 feuilles'
             }
         ],
-        temperature: [85, 90],
-        quantity: '1:50',
-        duration: [30, 60, 120],
-        times: '3 fois ou plus',
-        method: 'gaiwan'
+        brewing: {
+            granpa: {
+                temperature: [85, 90],
+                quantity: '1:50'
+            }
+        }
     },
     {
         type: 'blue',
         criteria: [
             {
-                cn:
-                    'mǐn běi wū lóng（yán chá)， guǎng dōng wū lóng (dān cōng）',
-                pinyin: '閩北烏龍 （岩茶)， 廣東烏龍（單樅）',
+                cn: 'mǐn běi wū lóng（yán chá)',
+                pinyin: '閩北烏龍 (岩茶)',
                 fr: 'sombre, brun, rouge'
             }
         ],
-        temperature: [95, 100],
-        quantity: '1:20',
-        duration: [25, 20, 25, 30, 40],
-        times: '5 fois ou plus',
-        method: 'theiere'
+        brewing: {
+            gaiwan: {
+                temperature: [95, 100],
+                quantity: '1:20',
+                duration: [15, 15, 15, 20, 25],
+                times: '5 fois ou plus'
+            }
+        }
     },
     {
         type: 'blue',
         criteria: [
             {
-                cn: 'mǐnnán wū lóng (Ānxī tiě guān yīn)，Táiwān wū lóng',
-                pinyin: '閩南烏龍 (安溪鐵觀音)，臺灣烏龍',
+                cn: 'guǎng dōng wū lóng (dān cōng）',
+                pinyin: '廣東烏龍 (單樅)',
+                fr: 'sombre, brun, rouge'
+            }
+        ],
+        brewing: {
+            gaiwan: {
+                temperature: [95, 100],
+                quantity: '1:20',
+                duration: [15, 15, 15, 20, 25],
+                times: '5 fois ou plus'
+            }
+        }
+    },
+    {
+        type: 'blue',
+        criteria: [
+            {
+                cn: 'mǐnnán wū lóng (Ānxī tiě guān yīn)',
+                pinyin: '閩南烏龍 (安溪鐵觀音)',
                 fr: 'vert clair, vert foncé'
             }
         ],
-        temperature: [90, 95],
-        quantity: '1:20',
-        duration: [20, 15, 20, 25, 35],
-        times: '4 fois ou plus',
-        method: 'theiere'
+        brewing: {
+            gaiwan: {
+                temperature: [90, 100],
+                quantity: '1:20',
+                duration: [20, 15, 20, 25, 35],
+                times: '4 fois ou plus'
+            },
+            teapot: {
+                temperature: [90, 100],
+                quantity: '1:20',
+                duration: [20, 15, 20, 25, 35],
+                times: '4 fois ou plus'
+            }
+        }
     },
     {
         type: 'blue',
         criteria: [
             {
-                cn: 'oriental beauty',
-                pinyin: 'oriental beauty',
+                cn: 'Táiwān wū lóng',
+                pinyin: '臺灣烏龍',
+                fr: 'vert clair, vert foncé'
+            }
+        ],
+        brewing: {
+            gaiwan: {
+                temperature: [90, 100],
+                quantity: '1:20',
+                duration: [20, 15, 20, 25, 35],
+                times: '4 fois ou plus'
+            },
+            teapot: {
+                temperature: [90, 100],
+                quantity: '1:20',
+                duration: [20, 15, 20, 25, 35],
+                times: '4 fois ou plus'
+            },
+            zhong: {
+                temperature: [90, 100],
+                quantity: '1:20',
+                duration: [20, 15, 20, 25, 35],
+                times: '4 fois ou plus'
+            }
+        }
+    },
+    {
+        type: 'blue',
+        criteria: [
+            {
+                cn: 'dong fang mei ren',
+                pinyin: '',
                 fr: 'oriental beauty'
             }
         ],
-        temperature: [85, 90],
-        quantity: '1:20',
-        duration: [25, 20, 25, 30, 40],
-        times: '5 fois ou plus',
-        method: 'gaiwan'
+        brewing: {
+            gaiwan: {
+                temperature: [85, 95],
+                quantity: '1:20',
+                duration: [25, 20, 25, 30, 40],
+                times: '5 fois ou plus'
+            }
+        }
     },
     {
         type: 'red',
         criteria: [
             {
-                cn: 'xiǎo zhǒng hóng chá, gōng fū hóng chá',
-                pinyin: '小種紅茶, 工夫紅茶',
+                cn: 'xiǎo zhǒng hóng chá',
+                pinyin: '小種紅茶',
                 fr: 'feuilles entières'
             }
         ],
-        temperature: [85, 100],
-        quantity: '1:20',
-        duration: [20, 25, 30, 20],
-        times: '3 ou 4 fois',
-        method: 'gaiwan'
+        brewing: {
+            gaiwan: {
+                temperature: [85, 95],
+                quantity: '1:20',
+                duration: [25, 25, 30, 40],
+                times: '3 ou 4 fois'
+            }
+        }
+    },
+    {
+        type: 'red',
+        criteria: [
+            {
+                cn: 'gōng fū hóng chá',
+                pinyin: '工夫紅茶',
+                fr: 'feuilles entières'
+            }
+        ],
+        brewing: {
+            gaiwan: {
+                temperature: [85, 95],
+                quantity: '1:20',
+                duration: [25, 25, 30, 40],
+                times: '3 ou 4 fois'
+            }
+        }
     },
     {
         type: 'red',
@@ -162,11 +277,12 @@ const brewList = [
                 fr: 'feuilles concassées ou en perle'
             }
         ],
-        temperature: [85],
-        quantity: '1:50',
-        duration: [160],
-        times: "1 fois et rajouter de l'eau",
-        method: 'mug'
+        brewing: {
+            granpa: {
+                temperature: [85],
+                quantity: '1:50'
+            }
+        }
     },
     {
         type: 'black',
@@ -177,11 +293,14 @@ const brewList = [
                 fr: 'thé noir'
             }
         ],
-        temperature: [100],
-        quantity: '1:100',
-        duration: [1200],
-        times: '1 fois',
-        method: 'bouilloire'
+        brewing: {
+            kettle: {
+                temperature: [100],
+                quantity: '1:100',
+                duration: [1200],
+                times: '1 fois'
+            }
+        }
     },
     {
         type: 'black',
@@ -192,16 +311,20 @@ const brewList = [
                 fr: 'pu er cuit'
             }
         ],
-        temperature: [100],
-        quantity: '1:100',
-        duration: [1200],
-        times: 'une fois',
-        method: 'bouilloire'
+        brewing: {
+            kettle: {
+                temperature: [100],
+                quantity: '1:100',
+                duration: [1200],
+                method: 'bouilloire'
+            }
+        }
     }
 ]
 
 const tea = {
     typeList: typeList,
+    brewMethodList: brewMethodList,
     brewList: brewList
 }
 
