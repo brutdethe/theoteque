70e09bd8e9801b39fecf7f846c2d457ccf147ed5
pntbr <pntbr@pm.me>
2020-04-13 13:44:37 +0200

    ajoute brewStyle au -store- et passe les valeurs par défaut en chaine vide

--- src/stores.js
+++ src/stores.js
@@ -1,8 +1,9 @@
 import { writable, derived } from 'svelte/store'
 import tea from './teaData.js'
 
-export const type = writable(undefined)
-export const criteria = writable(undefined)
+export const type = writable('')
+export const criteria = writable('')
+export const brewStyle = writable('')
 
 export const teaGroup = derived([type, criteria], $selected => {
     if ($selected[0] && Number.isInteger($selected[1])) {
@@ -10,6 +11,6 @@ export const teaGroup = derived([type, criteria], $selected => {
             $selected[1]
         ]
     } else {
-        return undefined
+        return ''
     }
 })
