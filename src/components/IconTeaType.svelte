a4a9a7f059d962c886f2fb88c2c33ded22cfb38e
pntbr <pntbr@pm.me>
2020-04-28 03:27:23 +0200

    ajoute une couleur pour les thés

---
+++ src/components/IconTeaType.svelte
@@ -0,0 +1,24 @@
+<style>
+    span {
+        display: block;
+        border: 1px solid black;
+        width: 1em;
+        height: 1em;
+        margin-top: 0.5em;
+    }
+</style>
+
+<script>
+    import { i18n } from '../stores.js'
+
+    export let type
+
+    function getColor(type, i18n) {
+        if (i18n.hasOwnProperty(type)) {
+            return i18n[type].color
+        }
+        return 'white'
+    }
+</script>
+
+<span style="background: {getColor(type, $i18n)}"></span>
