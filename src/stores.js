40e714cb040d071aa42ce2240bea93f94c6a318d
pntbr <pntbr@pm.me>
2020-04-28 13:37:34 +0200

    corrige un bug en changeant les format des requètes fetch

--- src/stores.js
+++ src/stores.js
@@ -1,16 +1,30 @@
 import { readable } from 'svelte/store'
 import yaml from 'js-yaml'
 
-export const teas = readable([], set =>
+export const teas = readable([], set => {
     fetch('./teas.yaml').then(response => response.text()).then(teas =>
         set(yaml.safeLoad(teas))
     ).catch((error) => {
         throw new Error(error)
-    }))
+    })
+})
 
-export const i18n = readable([], set =>
+export const i18n = readable([], set => {
     fetch('./teaTranslate.yaml').then(response => response.text()).then(texts =>
         set(yaml.safeLoad(texts))
     ).catch((error) => {
         throw new Error(error)
-    }))
+    })
+})
+
+/*
+export const teas = readable([], async set => {
+    const response = await fetch('./teas.yaml')
+
+    if (response.ok) {
+        set(yaml.safeLoad(await response.text()))
+    } else {
+        throw new Error(error)
+    }
+})
+*/
