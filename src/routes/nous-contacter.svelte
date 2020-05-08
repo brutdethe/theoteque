1e3c1ea4c979bd442178e2d3bccad7b284b7a5c6
pntbr <pntbr@pm.me>
2020-05-08 03:00:14 +0200

    adds "nous contacter"

---
+++ src/routes/nous-contacter.svelte
@@ -0,0 +1,23 @@
+<h2>Nous contacter</h2>
+
+<p>
+    Tu peux nous poser des questions par mail On pourra par la suite soit se
+    rencontrer en présentiel ou bien en visio-conférence
+</p>
+
+<h2>Par email</h2>
+Au choix : - [Stéphane](mailto:stephane@scopyleft.fr) -
+[Thaïs](mailto:thais.e@hotmail.fr) - [Ya-Lin](mailto:bizienyalin@gmail.com)
+<h3>Sur Mastodon</h3>
+- [Stéphane](https://eldritch.cafe/@pntbr) C'est quoi
+[mastodon](https://fr.wikipedia.org/wiki/Mastodon_(r%C3%A9seau_social)) ?
+<h3>Avec Slack</h3>
+<p>
+    Tu peux aussi demander à nous rejoindre pour échanger avec nous :
+    <a href="https://oisiflorus.slack.com">oisiflorus.slack.com</a>
+</p>
+<p>
+    C'est quoi
+    <a href="https://fr.wikipedia.org/wiki/Slack">slack</a>
+    ?
+</p>
