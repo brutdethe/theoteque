ececb03a74cd49f011ae925e8fce8f12542cc666
pntbr <pntbr@pm.me>
2020-06-07 18:11:32 +0200

    reprise de la miseen forme de la liste

--- src/routes/documentation/index.svelte
+++ src/routes/documentation/index.svelte
@@ -35,39 +35,41 @@
     <title>Documentation sur les thés</title>
 </svelte:head>
 
-<section id="ghTree" class="ghTree" data-title="tree">
-    <header>
-        <h1>Liste des articles</h1>
-    </header>
+<article class="blobContent" data-title="content">
+    <section id="ghTree" class="ghTree" data-title="tree">
+        <header>
+            <h1>Liste des articles</h1>
+        </header>
 
-    {#each posts as post}
-        <!-- we're using the non-standard `rel=prefetch` attribute to
+        {#each posts as post}
+            <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
 
-        <article class="ghTreeItem ghTypeFile" data-title="dir">
-            <h2 class="ghTreeTitle">
+            <article class="ghTreeItem ghTypeFile" data-title="dir">
+                <h2 class="ghTreeTitle">
+                    <a
+                        rel="prefetch"
+                        class="folderLink"
+                        data-title="folderLink"
+                        href="documentation/{post.lien}"
+                    >
+                        {post.titre}
+                    </a>
+                </h2>
+                <p class="ghTreeExcerpt" data-title="fileExcerpt">
+                    {post.description}
+                </p>
                 <a
-                    rel="prefetch"
-                    class="folderLink"
-                    data-title="folderLink"
+                    class="ghTreeReadmore"
+                    title="Lire la suite de la fiche : {post.titre}"
+                    data-title="fileReadmoreLink"
                     href="documentation/{post.lien}"
                 >
-                    {post.titre}
+                    Lire la suite de la fiche
                 </a>
-            </h2>
-            <p class="ghTreeExcerpt" data-title="fileExcerpt">
-                {post.description}
-            </p>
-            <a
-                class="ghTreeReadmore"
-                title="Lire la suite de la fiche : {post.titre}"
-                data-title="fileReadmoreLink"
-                href="documentation/{post.lien}"
-            >
-                Lire la suite de la fiche
-            </a>
-        </article>
-    {/each}
-</section>
+            </article>
+        {/each}
+    </section>
+</article>
